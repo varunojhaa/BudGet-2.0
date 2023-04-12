6be54650c829e7e111b6fa9998a8d17d67ba0842
varunojhaa <varunojha77@gmail.com>
2023-04-12 14:51:20 +0530

    04

--- f/src/App.js
+++ f/src/App.js
@@ -1,105 +1,38 @@
-import React, { useState, useMemo } from 'react';
+import React, {useState, useMemo} from 'react'
 import styled from "styled-components";
-import bg from './img/bg.png';
-import { MainLayout } from './styles/Layouts';
-import Orb from './Components/Orb/Orb';
-import Navigation from './Components/Navigation/Navigation';
+import bg from './img/bg.png'
+import {MainLayout} from './styles/Layouts'
+import Orb from './Components/Orb/Orb'
+import Navigation from './Components/Navigation/Navigation'
 import Dashboard from './Components/Dashboard/Dashboard';
-import Income from './Components/Income/Income';
+import Income from './Components/Income/Income'
 import Expenses from './Components/Expenses/Expenses';
 import { useGlobalContext } from './context/globalContext';
 
 function App() {
-  const [active, setActive] = useState(1);
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [signUp, setSignUp] = useState(false);
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [active, setActive] = useState(1)
 
-  const global = useGlobalContext();
+  const global = useGlobalContext()
   console.log(global);
 
   const displayData = () => {
-    switch (active) {
+    switch(active){
       case 1:
+        return <Dashboard />
+      case 2:
         return <Dashboard />
       case 3:
         return <Income />
-      case 4:
+      case 4: 
         return <Expenses />
-      default:
+      default: 
         return <Dashboard />
     }
-  }
-
-  const handleLogin = (event) => {
-    event.preventDefault();
-  
-    // Get the username and password from the input fields
-    const inputUsername = event.target.querySelector('input[type="text"]').value;
-    const inputPassword = event.target.querySelector('input[type="password"]').value;
-  
-    // Check if the username and password match with the stored user data
-    const userData = JSON.parse(localStorage.getItem('userData'));
-    if (userData && userData.username === inputUsername && userData.password === inputPassword) {
-      setLoggedIn(true);
-    } else {
-      alert('Invalid username or password.');
-    }
-  }
-
-  const handleSignUp = (event) => {
-    event.preventDefault();
-    // Save the new user data in local storage
-    localStorage.setItem('userData', JSON.stringify({ username, password }));
-    setSignUp(false);
-    setUsername('');
-    setPassword('');
   }
 
   const orbMemo = useMemo(() => {
     return <Orb />
-  }, []);
-
-  if (signUp) {
-    return (
-      <LoginPageStyled bg={bg} className="LoginPage">
-        <h1>Sign Up</h1>
-        <form onSubmit={handleSignUp}>
-          <label>
-            Username:
-            <input type="text" value={username} onChange={(event) => setUsername(event.target.value)} />
-          </label>
-          <label>
-            Password:
-            <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
-          </label>
-          <button type="submit">Sign Up</button>
-          <button onClick={() => setSignUp(false)}>Cancel</button>
-        </form>
-      </LoginPageStyled>
-    );
-  }
-
-  if (!loggedIn) {
-    return (
-      <LoginPageStyled bg={bg} className="LoginPage">
-        <h1>Login Page</h1>
-        <form onSubmit={handleLogin}>
-          <label>
-            Username:
-            <input type="text" />
-          </label>
-          <label>
-            Password:
-            <input type="password" />
-          </label>
-          <button type="submit">Login</button>
-        </form>
-        <button onClick={() => setSignUp(true)}>Sign Up</button>
-      </LoginPageStyled>
-    );
-  }
+  },[])
 
   return (
     <AppStyled bg={bg} className="App">
@@ -114,114 +47,21 @@ function App() {
   );
 }
 
-const LoginPageStyled = styled.div`
+const AppStyled = styled.div`
   height: 100vh;
   background-image: url(${props => props.bg});
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  h1 {
-    margin-bottom: 20px;
-    animation: slideInDown 0.5s ease;
-  }
-
-  form {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    animation: slideInUp 0.5s ease;
-    
-    label {
-      display: flex;
-      flex-direction: column;
-      margin-bottom: 10px;
-    
-      input {
-        padding: 5px;
-        margin-top: 5px;
-        border: none;
-        border-bottom: 2px solid #0077FF;
-        transition: border-color 0.2s ease;
-        
-        &:focus {
-          border-color: #005FCC;
-        }
-      }
-    }
-    
-    button {
-      padding: 10px;
-      margin-top: 20px;
-      background-color: #0077FF;
-      color: #fff;
-      border: none;
-      cursor: pointer;
-      transition: background-color 0.2s ease;
-    
-      &:hover {
-        background-color: #005FCC;
-      }
-    }
-  }
-  
-  button {
-    margin-top: 20px;
-    padding: 10px;
-    background-color: #fff;
-    border: none;
-    cursor: pointer;
-    text-decoration: underline;
-    color: #0077FF;
-    animation: slideInUp 0.5s ease;
-
-    &:hover {
-      color: #005FCC;
-    }
-  }
-
-  /* Keyframe animations */
-
-  @keyframes slideInDown {
-    from {
-      transform: translateY(-50%);
-      opacity: 0;
-    }
-    to {
-      transform: translateY(0);
-      opacity: 1;
-    }
-  }
-
-  @keyframes slideInUp {
-    from {
-      transform: translateY(50%);
-      opacity: 0;
-    }
-    to {
-      transform: translateY(0);
-      opacity: 1;
+  position: relative;
+  main{
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #FFFFFF;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    overflow-x: hidden;
+    &::-webkit-scrollbar{
+      width: 0;
     }
   }
 `;
-
-
-const AppStyled = styled.div`
-    height: 100vh;
-    background-image: url(${(props) => props.bg});
-    position: relative;
-    main {
-      flex: 1;
-      background: rgba(252, 246, 249, 0.78);
-      border: 3px solid #ffffff;
-      backdrop-filter: blur(4.5px);
-      border-radius: 32px;
-      overflow-x: hidden;
-      &::-webkit-scrollbar {
-        width: 0;
-      }
-    }
-  `;
 
 export default App;
--- f/src/Components/Income/Income.js
+++ f/src/Components/Income/Income.js
@@ -1,67 +1,76 @@
-import React, {useState, useMemo} from 'react'
-import styled from "styled-components";
-import bg from './img/bg.png'
-import {MainLayout} from './styles/Layouts'
-import Orb from './Components/Orb/Orb'
-import Navigation from './Components/Navigation/Navigation'
-import Dashboard from './Components/Dashboard/Dashboard';
-import Income from './Components/Income/Income'
-import Expenses from './Components/Expenses/Expenses';
-import { useGlobalContext } from './context/globalContext';
+import React, { useEffect } from 'react'
+import styled from 'styled-components'
+import { useGlobalContext } from '../../context/globalContext';
+import { InnerLayout } from '../../styles/Layouts';
+import Form from '../Form/Form';
+import IncomeItem from '../IncomeItem/IncomeItem';
 
-function App() {
-  const [active, setActive] = useState(1)
+function Income() {
+    const {addIncome,incomes, getIncomes, deleteIncome, totalIncome} = useGlobalContext()
 
-  const global = useGlobalContext()
-  console.log(global);
-
-  const displayData = () => {
-    switch(active){
-      case 1:
-        return <Dashboard />
-      case 2:
-        return <Dashboard />
-      case 3:
-        return <Income />
-      case 4: 
-        return <Expenses />
-      default: 
-        return <Dashboard />
-    }
-  }
-
-  const orbMemo = useMemo(() => {
-    return <Orb />
-  },[])
-
-  return (
-    <AppStyled bg={bg} className="App">
-      {orbMemo}
-      <MainLayout>
-        <Navigation active={active} setActive={setActive} />
-        <main>
-          {displayData()}
-        </main>
-      </MainLayout>
-    </AppStyled>
-  );
+    useEffect(() =>{
+        getIncomes()
+    }, [])
+    return (
+        <IncomeStyled>
+            <InnerLayout>
+                <h1>Incomes</h1>
+                <h2 className="total-income">Total Income: <span>₹{totalIncome()}</span></h2>
+                <div className="income-content">
+                    <div className="form-container">
+                        <Form />
+                    </div>
+                    <div className="incomes">
+                        {incomes.map((income) => {
+                            const {_id, title, amount, date, category, description, type} = income;
+                            return <IncomeItem
+                                key={_id}
+                                id={_id} 
+                                title={title} 
+                                description={description} 
+                                amount={amount} 
+                                date={date} 
+                                type={type}
+                                category={category} 
+                                indicatorColor="var(--color-green)"
+                                deleteItem={deleteIncome}
+                            />
+                        })}
+                    </div>
+                </div>
+            </InnerLayout>
+        </IncomeStyled>
+    )
 }
 
-const AppStyled = styled.div`
-  height: 100vh;
-  background-image: url(${props => props.bg});
-  position: relative;
-  main{
-    flex: 1;
-    background: rgba(252, 246, 249, 0.78);
-    border: 3px solid #FFFFFF;
-    backdrop-filter: blur(4.5px);
-    border-radius: 32px;
-    overflow-x: hidden;
-    &::-webkit-scrollbar{
-      width: 0;
+const IncomeStyled = styled.div`
+    display: flex;
+    overflow: auto;
+    .total-income{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: #FCF6F9;
+        border: 2px solid #FFFFFF;
+        box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+        border-radius: 20px;
+        padding: 1rem;
+        margin: 1rem 0;
+        font-size: 2rem;
+        gap: .5rem;
+        span{
+            font-size: 2.5rem;
+            font-weight: 800;
+            color: var(--color-green);
+        }
     }
-  }
+    .income-content{
+        display: flex;
+        gap: 2rem;
+        .incomes{
+            flex: 1;
+        }
+    }
 `;
 
-export default App;
+export default Income
