e69b488867b5f1ff162afbb23f3be8284c860984
varunojhaa <varunojha77@gmail.com>
2023-04-12 14:26:51 +0530

    Start

---
+++ f/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { GlobalProvider } from './context/globalContext';
+import { GlobalStyle } from './styles/GlobalStyle';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <GlobalStyle />
+    <GlobalProvider>
+      <App />
+    </GlobalProvider>
+  </React.StrictMode>
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
